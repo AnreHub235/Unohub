d5b332d675af979b94c46c72bcc4651e96f6309f
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-23 23:05:09 +0700

    Update Status.js

--- API/Status.js
+++ API/Status.js
@@ -1,7 +1,7 @@
 
-    Fluxus: "Died 🔴"
-    Arceus: "Working🟢"
-    Delta: "Send Some user. Bypass BOT doesn't has API bypass🟡"
-    Trigon: "Send Some user. Bypass BOT doesn't has API bypass🟡"
-    Hydrogen: "Send Some user. Bypass BOT doesn't has API bypass🟡"
-    Codex: "Send Some user. Bypass BOT doesn't has API bypass🟡"
+    :Fluxus: "Died 🔴"
+    :Arceus: "Working🟢"
+    :Delta: "Send Some user. Bypass BOT doesn't has API bypass🟡"
+    :Trigon: "Send Some user. Bypass BOT doesn't has API bypass🟡"
+    :Hydrogen: "Send Some user. Bypass BOT doesn't has API bypass🟡"
+    :Codex: "Send Some user. Bypass BOT doesn't has API bypass🟡"
