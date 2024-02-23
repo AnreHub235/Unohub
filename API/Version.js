49bde25f0f8b16e726ba6795f92b38c2fb89004e
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-23 22:57:38 +0700

    Create Version.js

---
+++ API/Version.js
@@ -0,0 +1,54 @@
+{"Codex":
+ {"IOS":
+     "2.612"    
+ },
+ {"Android":
+     "2.610"    
+ },
+},
+
+{"Fluxus":
+ {"IOS":
+     "Not Found"
+ },
+ {"Android":
+     "2.610_32bit.apk",
+     "2.610_64bit"
+ }
+},
+
+{"Delta":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "Cannot check version by link discord invalid"
+ },
+},
+
+{"Hydrogen":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "v2.6.4"
+ },
+},
+
+{"Vegax":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "IDK"
+ },
+},
+
+{"ArceusX":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "V1.2.1"
+ },
+},
