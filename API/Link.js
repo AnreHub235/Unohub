57b88c69825758acc8be1daa2dd6f84d1ca35fe9
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-23 22:47:41 +0700

    Create Link.js

---
+++ API/Link.js
@@ -0,0 +1,54 @@
+{"Codex":
+ {"IOS":
+     "https://www.mediafire.com/file/3mbp8j9ozhx0pmm/CodexIOS.ipa/file"    
+ },
+ {"Android":
+     "https://www.mediafire.com/file/a4ykl5l0a2negey/Codex_Police.apk/file"    
+ },
+},
+
+{"Fluxus":
+ {"IOS":
+     "Not Found"
+ },
+ {"Android":
+     "https://www.mediafire.com/file/0nmotlcysssoa9p/Original_Fluxus_2.610_32bit.apk/file",
+     "https://www.mediafire.com/file/eaj0vrznpl1wpi1/Original_Fluxus_2.610_64bit.apk/file"
+ }
+},
+
+{"Delta":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "https://www.mediafire.com/file/18g50unlpqjcccb/DELTA+2.610.472_signed+aligned.apk/file"
+ },
+},
+
+{"Hydrogen":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "https://rb.gy/62058"
+ },
+},
+
+{"Vegax":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "https://www.mediafire.com/file_premium/6bhmia2v10hf5z7/Vega_X_V4.apk/file"
+ },
+},
+
+{"ArceusX":
+ {"IOS": 
+     "Not Found"
+ },
+ {"Android": 
+     "https://arceusxneoexecutor.com/neo.apk"
+ },
+},
