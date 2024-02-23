ae94a7b017d7e6703528ce49295712a2ec1a5406
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-23 23:04:14 +0700

    Update Link.js

--- API/Link.js
+++ API/Link.js
@@ -1,54 +1,42 @@
-{"Codex":
- {"IOS":
-     "https://www.mediafire.com/file/3mbp8j9ozhx0pmm/CodexIOS.ipa/file"    
- },
- {"Android":
-     "https://www.mediafire.com/file/a4ykl5l0a2negey/Codex_Police.apk/file"    
- },
-},
+:Codex:
+ :IOS:
+ https://www.mediafire.com/file/3mbp8j9ozhx0pmm/CodexIOS.ipa/file
+ 
+ :Android:
+ https://www.mediafire.com/file/a4ykl5l0a2negey/Codex_Police.apk/file
 
-{"Fluxus":
- {"IOS":
-     "Not Found"
- },
- {"Android":
-     "https://www.mediafire.com/file/0nmotlcysssoa9p/Original_Fluxus_2.610_32bit.apk/file",
-     "https://www.mediafire.com/file/eaj0vrznpl1wpi1/Original_Fluxus_2.610_64bit.apk/file"
- }
-},
+:Fluxus:
+ :IOS:
+ Not Found
 
-{"Delta":
- {"IOS": 
-     "Not Found"
- },
- {"Android": 
-     "https://www.mediafire.com/file/18g50unlpqjcccb/DELTA+2.610.472_signed+aligned.apk/file"
- },
-},
+ :Android:
+     https://www.mediafire.com/file/0nmotlcysssoa9p/Original_Fluxus_2.610_32bit.apk/file
+     https://www.mediafire.com/file/eaj0vrznpl1wpi1/Original_Fluxus_2.610_64bit.apk/file
+ 
+:Delta:
+ :IOS: 
+     Not Found
+ 
+ :Android: 
+     https://www.mediafire.com/file/18g50unlpqjcccb/DELTA+2.610.472_signed+aligned.apk/file
 
-{"Hydrogen":
- {"IOS": 
-     "Not Found"
- },
- {"Android": 
-     "https://rb.gy/62058"
- },
-},
+:Hydrogen:
+ :IOS: 
+     Not Found
 
-{"Vegax":
- {"IOS": 
-     "Not Found"
- },
- {"Android": 
-     "https://www.mediafire.com/file_premium/6bhmia2v10hf5z7/Vega_X_V4.apk/file"
- },
-},
+ :Android: 
+     https://rb.gy/62058
 
-{"ArceusX":
- {"IOS": 
-     "Not Found"
- },
- {"Android": 
-     "https://arceusxneoexecutor.com/neo.apk"
- },
-},
+:Vegax:
+ :IOS: 
+     Not Found
+ 
+ :Android: 
+     https://www.mediafire.com/file_premium/6bhmia2v10hf5z7/Vega_X_V4.apk/file
+
+:ArceusX:
+ :IOS: 
+     Not Found
+
+ :Android: 
+     https://arceusxneoexecutor.com/neo.apk
