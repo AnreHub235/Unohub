5baf705650795f96839646b6ca86ab7e539ae5f1
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-25 01:56:17 +0700

    Create PetSimulator.js

---
+++ API/Script/PetSimulator.js
@@ -0,0 +1,64 @@
+-- Zap Hub
+```lua
+loadstring(game:HttpGet('https://www.zaphub.xyz/Exec'))()
+```
+
+Link Key: https://linkvertise.com/608384/zaphub-key-system
+
+-- Zer0 Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Discord0000/Zer0Hub/main/MainScript.lua"))()
+```
+
+Link Key: https://zer0hub.com/
+
+-- Nicuse Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Nicuse/RobloxScripts/main/PetSimulator99.lua"))()
+```
+
+Link Key: https://nicuse.xyz/getkey
+
+-- Remove Egg Hatch Animation
+```lua
+local Eggs = game.Players.LocalPlayer.PlayerScripts.Scripts.Game['Egg Opening Frontend']getsenv(Eggs).PlayEggAnimation = function() return end
+```
+
+-- BT Project
+```lua
+loadstring(game:HttpGet('https://btteam.top/scripts-auth', true))()
+```
+
+Link key: https://btteam.top/checkpoint/checkpoint-1.php
+
+-- Nex Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CopyReal/NexHub/main/NexHubLoader", true))()
+```
+
+Link Key: discord.com/invite/jkscZr4wxw (FU Link)
+
+-- Versus Hub
+```lua
+loadstring(game:HttpGet("https://gitlab.com/Ner0ox/versus/-/raw/main/LoaderV2/Loader.lua"))()
+```
+
+-- Dex Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BinaryDex/DexHub/main/Init"))()
+```
+
+-- Celestial Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JellyBagBeans/CelestialHub/main/GameLoader"))()
+```
+
+-- EzScripts Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TurfuFrogy/EzScripts/main/loader.lua"))()
+```
+
+-- RS Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MHD-444/RS-HUB/main/KEY%20SYS"))()
+```
