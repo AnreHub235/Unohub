9261015b6256d48c8a128b68c7a199d5925525d9
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-25 01:52:18 +0700

    Create Bladeball.js

---
+++ API/Script/Bladeball.js
@@ -0,0 +1,78 @@
+-- W-azure Hub
+```lua
+getgenv().Mode = "AI" -- "AimOnly","Rage", "AI"
+getgenv().ForceWin = false --100% Win Need Telekenis
+getgenv().AutoUseSkill = true
+getgenv().BaseVelocity = 0 --Base On The Server, IDK Where To Get it It Should Be 4-6 Or 0 (Old Server)
+getgenv().BasePredictVelocity = 3.4 -- Should be From 3-3.5 Work Good With 3 If Old Server
+getgenv().VisualizePath = true -- false //
+getgenv().AutoSpamClickDetect = true -- Spam Click If Near Other Player
+getgenv().CloseRangeAttack = true -- Just Move Forward To The Last Character
+getgenv().AutoGetVelocity = false -- Need The Ball To Be Stopped
+getgenv().AutoClickKeyBind = "X" -- Need The Ball To Be Stopped
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/fd07660d92cb26891e9acfab9f0c6ba4.lua"))()
+```
+
+-- Yon v3 (Died)
+```
+local UILib = loadstring(game:HttpGet('https://raw.githubusercontent.com/StepBroFurious/Script/main/HydraHubUi.lua'))()
+local Window = UILib.new("Blade Ball", game.Players.LocalPlayer.UserId, "NXT. Member")
+local Category1 = Window:Category("Main", "http://www.roblox.com/asset/?id=8395621517")
+local SubButton1 = Category1:Button("Combat", "http://www.roblox.com/asset/?id=8395747586")
+local Section1 = SubButton1:Section("Section", "Left")
+```
+
+-- Nova Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Synergy-Networks/products/main/ProjectNova/loader.lua",true))()
+```
+
+-- Alienware Hub
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/romkich09/Ball/main/Balde")))()
+```
+
+-- R3TH-PRIV Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/R3TH-PRIV/R3THPRIV/main/loader.lua'))()
+```
+
+-- Lightux Hub
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/zeuise0002/SSSWWW222/main/README.md")))()
+```
+
+Link Key: [LMAO CLICK ME TO GETKEY](https://prxobanana2.wixsite.com/lightux)
+
+-- Red Circle Auto Parry (Press X to spam LMAO)
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UltraStuff/scripts2/main/bladelua")))()
+```
+
+```lua
+getgenv().visualizer = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/RedCircleBlock"))()
+```
+
+-- LuaF Hub
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UltraStuff/scripts2/main/bladelua")))()
+```
+
+-- Bedol Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/-beta-/main/AutoParry.lua"))()
+```
+
+-- Inferno Hub
+```lua
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UltraStuff/scripts2/main/blademain")))()
+```
+
+-- Visual Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevTravDYT/VisualProductV10/main/KeySystem/ThankForUsing!/by-d-e-v-t-r-a-v/lua",true))()
+```
+
+-- Link Key: Unknown
