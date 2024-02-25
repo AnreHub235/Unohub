5461cafb809d560e02063552c73106ab2fa5253e
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-25 12:14:07 +0700

    Create BladeBall.js

---
+++ API/Leaderboard/BladeBall.js
@@ -0,0 +1,19 @@
+Leaderboard Script :
+Name:                              Has:                              Key:
+W-azure                                  1                                  Yes (I ment key Paid)
+Yon v3                                   1                                  Yes
+Nova                                     1                                  IDK
+Alienware                                1                                  IDK
+R3TH-PRIV                                1                                  IDK
+Lightux                                  1                                  Yes
+Red Circle                               2                                  No
+LuaF                                     1                                  No
+Bedol                                    1                                  No
+Inferno                                  1                                  IDK
+Visual                                   1                                  Yes(i ment key in discord)
+
+Total Script: 
+   11 Hub LMAO
+   4 Hub has Key
+   3 Hub No Key
+   4 Hub IDK has key or Nothing
