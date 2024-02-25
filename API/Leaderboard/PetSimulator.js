37c888f042317567ea720e68b002d78238591812
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-02-25 12:15:08 +0700

    Create PetSimulator.js

---
+++ API/Leaderboard/PetSimulator.js
@@ -0,0 +1,19 @@
+Leaderboard Script :
+Name:                              Has:                              Key:
+Zap                                      1                                   IDK
+Zer0                                     1                                   Yes
+Nicuse                                   1                                   Yes
+REHA                                     1                                   No
+BT Project                               1                                   Yes
+Nex                                      1                                   Yes
+Versus                                   1                                   IDK
+Dex                                      1                                   No
+Celestial                                1                                   IDK
+EzScripts                                1                                   IDK
+RS                                       1                                   IDK
+
+Total Script: 
+   11 Hub LMAO
+   4 Hub has Key
+   2 Hub No Key
+   5 Hub IDK has key or Nothing
