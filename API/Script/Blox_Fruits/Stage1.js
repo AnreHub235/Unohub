7385b4ff85850e9628f979b4c2d515ad17a2104b
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:32:01 +0700

    Create Stage1.js

---
+++ API/Script/Blox_Fruits/Stage1.js
@@ -0,0 +1,110 @@
+-- Hoho Hub
+```lua
+_G.HohoVersion = "v3"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HOHO_H/main/Loading_UI'))()
+```
+
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/acsu123/HohoV2/main/OldScriptLoad'))()
+```
+
+-- Uranium Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Augustzyzx/UraniumMobile/main/UraniumKak.lua"))()
+```
+-- CFrame Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruitV2.lua"))()
+```
+
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CFrame3310/CFrameHub/main/BloxFruit_Mobile.lua"))()()
+```
+-- Neva Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VEZ2/NEVAHUB/main/2'))()
+```
+
+-- Makima Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Piwwy0909/MakimaRemake/main/Cumback.Makima'))()
+```
+
+-- Sonic Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/Protected-25.lua"))()
+```
+
+-- Zee Hub
+```lua
+loadstring(game:HttpGet("https://zKuzy.link/ZeeHub.lua"))()
+```
+
+-- Madox Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Fiend1sh/FiendMain/main/MadoxHubKey", true))()
+```
+
+-- Unique Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AkiraNus/UniquehubKak/main/FreeCr.Xenonhub"))() 
+```
+
+-- Chiba Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/KindIhave/ChibaHubcomeback/main/Chibacomebackbeta.txt'))()
+```
+
+-- Zaque Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mei2232/ZaqueHub/main/Zaque%20Hub"))()
+```
+
+-- Glord Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GGhostVN/GhostLord/main/BF"))()
+```
+
+-- Blck Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/BLCK"))()
+```
+
+-- Loli Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/neon"))()
+```
+
+-- Alucard Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/JJTChannel/Alucard_hub/main/main.lua"))()
+```
+
+--------------
+
+-- Mukuro Hub (Require Key)
+```lua
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+```
+
+Link Key : [LMAO CLICK ME TO GETKEY](https://quartyz.dev)
+Warning: "Link only has 1 checkpoint don't ask 'require-keysystem' by link key send whitelist in NetworkIP to cannot whitelist to you"
+
+--Zen Hub (Require Key)
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader'))()
+```
+
+Link Key: "Unknown"
+
+-- Ren Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UltraStuff/scripts2/main/ren", true))()
+```
+
+-- Void Hub (OP - Require Key)
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/VoidHub/main/Script"))()
+```
+
+Link Key: "Unknown"
