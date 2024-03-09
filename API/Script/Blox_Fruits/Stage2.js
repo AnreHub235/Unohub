c921dc979be2fe2ef2f95c43f89f5f5576e55a90
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:32:32 +0700

    Create Stage2.js

---
+++ API/Script/Blox_Fruits/Stage2.js
@@ -0,0 +1,55 @@
+
+-- Blacktrap (OP - Require Key)
+```lua
+loadstring(game:HttpGetAsync("https://gitlab.com/Sky2836/test/-/raw/main/Script_All_In-One"))()
+```
+
+Link Key: "Unknown"
+
+-- Destiny Hub (Huh + Mobile) (Key)
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KudoCon/DestinyHub/main/DestinyKey"))()
+```
+
+Link Key: "Unknown"
+
+-- Atomic Hub (Huh + Mobile)
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArceusXHub/atomic-hub/main/atomic-hub.lua"))()
+```
+
+-- Zamex Hub (Best Ur Phone LOL)
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Sixnumz/ZamexMobile/main/Zamex_Mobile.lua'))()
+```
+
+-- Saki Hub (Huh + Mobile) (Key)
+```lua
+loadstring(game:HttpGetAsync('https://sharetext.me/raw/u3if6m3hgr'))()
+```
+
+Link Key: "Unknown"
+
+-- Speed Hub X (Hub + Mobile)
+```lua
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ahmadsgamer2/Script--Game/main/Script%20Game"))()
+```
+
+--------------
+
+-- Fruit FInder & Hopper
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/ChestFarmOp"))()
+```
+
+-- Winnable Hub (LMAO THE KEY ">:(((" )
+```lua
+_G.Mode = "Normal" -- Aimbot / Normal
+		_G.Key = "" -- if you already have a key you can put it here
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/tyuiop/Fps/Loading.lua"))()
+```
+
+-- Shadow Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF"))()
+```
