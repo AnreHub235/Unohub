8e5906a788183719e3d7d2b0bf65b2eca6f31cfa
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:37:26 +0700

    Create Stage1.js

---
+++ API/Leaderboard/Blox_Fruit/Stage1.js
@@ -0,0 +1,29 @@
+Leaderboard Script :
+Name:                              Has:                              Key:
+Hoho                                  2                                   Yes
+Uranium                            1                                    IDK
+CFrame                             2                                   IDK
+Neva                                  1                                    IDK
+Makima                             1                                    IDK
+Sonic                                  1                                    NO
+Zee                                      1                                   IDK
+Madox                                1                                   IDK
+Unique                                1                                   IDK
+Chiba                                  1                                   IDK
+Zaque                                 1                                    IDK
+Glord                                  1                                    IDK
+Blck                                    1                                    IDK
+Loli                                      1                                    IDK
+Alucard                              1                                    IDK
+Mukuro                               1                                   Yes
+Zen                                       1                                   Yes
+Ren                                       1                                   IDK
+Void                                      1                                   Yes
+Blacktrap                           1                                   Yes
+Destiny                                1                                   Yes
+Atomic                                 1                                   IDK
+Zamex                                  1                                   IDK
+Saki                                       1                                   Yes
+Speed X                               1                                    IDK
+Fruit Find & Hopper         1                                    NO
+Winnable                              1                                    Yes (I ment key paid)
