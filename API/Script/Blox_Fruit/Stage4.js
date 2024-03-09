6e58760529a9d96b79151e516fc2036057288dc5
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:36:07 +0700

    Create Stage4.js

---
+++ API/Script/Blox_Fruit/Stage4.js
@@ -0,0 +1,77 @@
+-- Tuper X Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/TUPERX/TUPERX/Main/MenuBF/README.md'))()
+```
+
+-- Blaze X Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/WindowsXp12/BlxzeRBLX/main/BLxzeHub.lua"))()
+```
+
+-- Sxnumbz Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/sxnumz/Script/main/SxnumzHubFree.txt'))();
+```
+
+-- East Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Brineeee/EastHub/main/lua"))()
+```
+
+-- One X Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xOne2/One-x-HUB/main/README.md"))()
+```
+
+-- Putaya Hub
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Putaya/HITBOXKAK/main/PutayaFree"), true))();
+```
+
+-- Foxy Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/x2foxy/FoxyHubFreescript/main/Freescript"))()
+```
+
+-- Meta Ware
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/x2FIWz/SCRIPT/main/MOBILE/METAWARE.lua'))()
+```
+
+-- MTriet Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Minhtriettt/Free-Script/main/Main-V2.lua"))()
+```
+
+-- Lùa Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/LuaHub/retard/Loader"))()
+```
+
+-- Alchemy Hub
+```lua
+_G.Language = "EN" -- EN ( English ) or TH ( Thai )
+v=1;loadstring(game:HttpGet("https://alchemyhub.xyz/v2"))()
+```
+
+Chest Farm
+```lua
+v=3;loadstring(game:HttpGet("https://alchemyhub.xyz/v2"))()
+```
+
+-- Royx Hub
+```lua
+_G.Key = ""
+_G.DiscordId = ""
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))();
+```
+
+Key: Tự mà mua đâu ra có sẵn :)))
+
+-- W-azure Hub
+```lua
+getgenv().Team = "Pirates"
+getgenv().FixCrash = false -- Turn it On For Hopping Server, Improve Performance But Silent Aim On Mob And Player
+getgenv().FixCrash2 = false -- Turn it On For Hopping Server, Improve Performance But Will Remove Speed Changer
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3b2169cf53bc6104dabe8e19562e5cc2.lua"))()
+```
