86518017c4618e779b070041c1355e99e562e29c
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:33:06 +0700

    Create Stage3.js

---
+++ API/Script/Blox_Fruits/Stage3.js
@@ -0,0 +1,41 @@
+
+-- Playback X Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NeaPchX2/Playback-X-HUB/main/Protected.lua.txt"))()
+```
+
+-- Flare Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/flares"))()
+```
+
+-- Mango Hub
+```lua
+getgenv().WaterMark = false
+loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta"))()
+```
+
+-- Tawan Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt', true))()
+```
+
+-- Netna Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/katrina367/ScriptBlox-Projects/main/OblivionAutoEXEV1'))()
+```
+
+-- String Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StringV2/StringHub/main/BF.txt", true))()
+```
+
+-- SMZ Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Frerfgzz/free-script/main/SMZHUBv2BETA"))()
+```
+
+-- Maru Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/joi-droid/MaruHubBF/main/GOHANSSJ3'))()
+```
