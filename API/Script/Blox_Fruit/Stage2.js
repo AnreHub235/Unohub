28cf660c514091a2ef733c4d56186249ff489f6a
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:35:09 +0700

    Create Stage2.js

---
+++ API/Script/Blox_Fruit/Stage2.js
@@ -0,0 +1,62 @@
+-- Mukuro Hub (Require Key)
+```lua
+loadstring(game:HttpGet"https://raw.githubusercontent.com/xQuartyx/DonateMe/main/ScriptLoader")()
+```
+
+Link Key : [LMAO CLICK ME TO GETKEY](https://quartyz.dev)
+Warning: "Link only has 1 checkpoint don't ask 'require-keysystem' by link key send whitelist in NetworkIP to cannot whitelist to you"
+
+--Zen Hub (Require Key)
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kaizenofficiall/ZenHub/main/Loader'))()
+```
+
+Link Key: "Unknown"
+
+-- Ren Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/UltraStuff/scripts2/main/ren", true))()
+```
+
+-- Void Hub (OP - Require Key)
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Efe0626/VoidHub/main/Script"))()
+```
+
+Link Key: "Unknown"
+
+-- Blacktrap (OP - Require Key)
+```lua
+loadstring(game:HttpGetAsync("https://gitlab.com/Sky2836/test/-/raw/main/Script_All_In-One"))()
+```
+
+Link Key: "Unknown"
+
+-- Destiny Hub (Huh + Mobile) (Key)
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KudoCon/DestinyHub/main/DestinyKey"))()
+```
+
+Link Key: "Unknown"
+
+-- Atomic Hub (Huh + Mobile)
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ArceusXHub/atomic-hub/main/atomic-hub.lua"))()
+```
+
+-- Zamex Hub (Best Ur Phone LOL)
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Sixnumz/ZamexMobile/main/Zamex_Mobile.lua'))()
+```
+
+-- Saki Hub (Huh + Mobile) (Key)
+```lua
+loadstring(game:HttpGetAsync('https://sharetext.me/raw/u3if6m3hgr'))()
+```
+
+Link Key: "Unknown"
+
+-- Speed Hub X (Hub + Mobile)
+```lua
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ahmadsgamer2/Script--Game/main/Script%20Game"))()
+```
