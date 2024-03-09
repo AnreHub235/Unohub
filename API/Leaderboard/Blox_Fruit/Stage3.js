c2955211cb6c009eff83b49b43282d9ddeb2916f
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:38:17 +0700

    Create Stage3.js

---
+++ API/Leaderboard/Blox_Fruit/Stage3.js
@@ -0,0 +1,13 @@
+One X                                      1                                    IDK
+Putaya                                    1                                    IDK
+Foxy                                        1                                    IDK
+Meta                                        1                                   IDK
+MTriet                                     1                                    IDK
+Lùa                                           1                                   IDK
+Alchemy                                 2                                  Yes
+
+Total Script: 
+   50 Hub LMAO
+   9 Hub has Key
+   3 Hub No Key
+   38 Hub IDK has key or Nothing
