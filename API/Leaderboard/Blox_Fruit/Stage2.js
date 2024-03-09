1c50b558cb5ed2f16656295f9ae1b9d0e66da7cd
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:37:53 +0700

    Create Stage2.js

---
+++ API/Leaderboard/Blox_Fruit/Stage2.js
@@ -0,0 +1,16 @@
+Shadow                                1                                    IDK
+PlaybackX                           1                                    IDK
+Flare                                      1                                    IDK
+Mango                                   1                                    NO
+Tawan                                    1                                    IDK
+Netna                                     1                                    IDK
+String                                     1                                    IDK
+SMZ                                        1                                    IDK
+Maru                                       1                                   IDK (LMAO)
+Strike                                      1                                    IDK
+MrMaxNaja                           1                                    IDK
+Fusion                                     1                                    IDK
+Tuper X                                   1                                    IDK
+Blaze X                                   1                                    IDK
+Sxnumbz                                1                                    IDK
+East                                         1                                    IDK
