f815d64de2fda1b4cd2d74f17ff79759ca9cf287
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 18:02:36 +0700

    Create Stage2.js

---
+++ API/Script/Bladeball/Stage2.js
@@ -0,0 +1,40 @@
+
+-- Lightux Hub
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/zeuise0002/SSSWWW222/main/README.md")))()
+```
+
+Link Key: [LMAO CLICK ME TO GETKEY](https://prxobanana2.wixsite.com/lightux)
+
+-- Red Circle Auto Parry (Press X to spam LMAO)
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UltraStuff/scripts2/main/bladelua")))()
+```
+
+```lua
+getgenv().visualizer = true
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/RedCircleBlock"))()
+```
+
+-- LuaF Hub
+```lua
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UltraStuff/scripts2/main/bladelua")))()
+```
+
+-- Bedol Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/-beta-/main/AutoParry.lua"))()
+```
+
+-- Inferno Hub
+```lua
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/UltraStuff/scripts2/main/blademain")))()
+```
+
+-- Visual Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DevTravDYT/VisualProductV10/main/KeySystem/ThankForUsing!/by-d-e-v-t-r-a-v/lua",true))()
+```
+
+-- Link Key: Unknown
