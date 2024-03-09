8d883f76b5ac481ac30526da39ff6001b4cb68e8
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-09 12:35:44 +0700

    Create Stage3.js

---
+++ API/Script/Blox_Fruit/Stage3.js
@@ -0,0 +1,72 @@
+-- Fruit FInder & Hopper
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NickelHUBB/SonicTuru/main/ChestFarmOp"))()
+```
+
+-- Winnable Hub (LMAO THE KEY ">:(((" )
+```lua
+_G.Mode = "Normal" -- Aimbot / Normal
+		_G.Key = "" -- if you already have a key you can put it here
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/tyuiop/Fps/Loading.lua"))()
+```
+
+-- Shadow Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xPeachy/ShadowHub/main/BF"))()
+```
+
+-- Playback X Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NeaPchX2/Playback-X-HUB/main/Protected.lua.txt"))()
+```
+
+-- Flare Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/master/flares"))()
+```
+
+-- Mango Hub
+```lua
+getgenv().WaterMark = false
+loadstring(game:HttpGet("https://gitlab.com/L1ZOT/mango-hub/-/raw/main/Mango-Bloxf-Fruits-Beta"))()
+```
+
+-- Tawan Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/kill55547/TAWAN_HUB/main/hub.lua.txt', true))()
+```
+
+-- Netna Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/katrina367/ScriptBlox-Projects/main/OblivionAutoEXEV1'))()
+```
+
+-- String Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StringV2/StringHub/main/BF.txt", true))()
+```
+
+-- SMZ Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Frerfgzz/free-script/main/SMZHUBv2BETA"))()
+```
+
+-- Maru Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/joi-droid/MaruHubBF/main/GOHANSSJ3'))()
+```
+
+-- Strike Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/StormSKz12/StirkeHub1/main/Gameincluded"))()
+```
+
+-- MrMaxNaja Hub
+```lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MrMaxNaJaEz/New-MrMaxNaJa-NaJa/main/README.md"))()
+```
+
+-- Fusion Hub
+```lua
+loadstring(game:HttpGet('https://raw.githubusercontent.com/SHAREHACK/script/main/fusion1'))()
+```
