85516e6b465245f51d71810cfa8811571d6fd038
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-10 22:23:02 +0700

    Update Link.js

--- API/Link.js
+++ API/Link.js
@@ -1,15 +1,15 @@
 **__OFFICIAL VERSIONS__** [ORIGINAL VERSIONS]
 **LATEST VERSION** **v 2.614** [check6](https://cdn.discordapp.com/emojis/950297012753010698.gif?size=48&quality=lossless&name=check6)
 
-(https://cdn.discordapp.com/emojis/1175308654023557140.webp?size=48&quality=lossless&name=deltalogo) **__DELTA__** : https://link-center.net/1052601/delta-v2614
-(https://cdn.discordapp.com/emojis/1175308812576620634.webp?size=48&quality=lossless&name=hydrogenlogo) **__HYDROGEN__** : https://direct-link.net/1052601/hydrogen-v2614
-(https://cdn.discordapp.com/emojis/1175308708901830807.webp?size=48&quality=lossless&name=codexlogo) **__CODEX__** : https://direct-link.net/1052601/codex-v2614
-(https://cdn.discordapp.com/emojis/1187549923491729408.webp?size=48&quality=lossless&name=neologo) **__ARCEUS X NEO__** : https://link-target.net/1052601/arceus-x-neo-v2614
-(https://cdn.discordapp.com/emojis/1192995861832147004.webp?size=48&quality=lossless&name=evonlogo) **__EVON__** : https://direct-link.net/1052601/evon-v2614
-(https://cdn.discordapp.com/emojis/1206869032213684285.webp?size=48&quality=lossless&name=valyselogo) **__VALYSE__** : https://direct-link.net/1052601/valyse-v2614
-(https://cdn.discordapp.com/emojis/1209792076758126612.webp?size=48&quality=lossless&name=vegaxlogo) **__VEGA X__** : https://link-target.net/1052601/vega-x-v4-v2614
-(https://cdn.discordapp.com/emojis/1195893209881124925.webp?size=48&quality=lossless&name=trigonlogo) **__TRIGON__** : https://link-hub.net/1052601/trigon-v2614
+https://cdn.discordapp.com/emojis/1175308654023557140.webp?size=48&quality=lossless&name=deltalogo **__DELTA__** : https://link-center.net/1052601/delta-v2614
+https://cdn.discordapp.com/emojis/1175308812576620634.webp?size=48&quality=lossless&name=hydrogenlogo **__HYDROGEN__** : https://direct-link.net/1052601/hydrogen-v2614
+https://cdn.discordapp.com/emojis/1175308708901830807.webp?size=48&quality=lossless&name=codexlogo **__CODEX__** : https://direct-link.net/1052601/codex-v2614
+https://cdn.discordapp.com/emojis/1187549923491729408.webp?size=48&quality=lossless&name=neologo **__ARCEUS X NEO__** : https://link-target.net/1052601/arceus-x-neo-v2614
+https://cdn.discordapp.com/emojis/1192995861832147004.webp?size=48&quality=lossless&name=evonlogo **__EVON__** : https://direct-link.net/1052601/evon-v2614
+https://cdn.discordapp.com/emojis/1206869032213684285.webp?size=48&quality=lossless&name=valyselogo **__VALYSE__** : https://direct-link.net/1052601/valyse-v2614
+https://cdn.discordapp.com/emojis/1209792076758126612.webp?size=48&quality=lossless&name=vegaxlogo **__VEGA X__** : https://link-target.net/1052601/vega-x-v4-v2614
+https://cdn.discordapp.com/emojis/1195893209881124925.webp?size=48&quality=lossless&name=trigonlogo **__TRIGON__** : https://link-hub.net/1052601/trigon-v2614
 
--> (https://cdn.discordapp.com/emojis/1175308860655943710.webp?size=48&quality=lossless&name=fluxuslogo) **__FLUXUS__**  :
+-> https://cdn.discordapp.com/emojis/1175308860655943710.webp?size=48&quality=lossless&name=fluxuslogo) **__FLUXUS__**  :
 **__32bit__** : https://link-target.net/901202/official-fluxus-v2614-32
 **__64bit__** : https://direct-link.net/901202/official-fluxus-v2614-64
