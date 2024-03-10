2d9171696f70649064fed0f2c8d8ae6388a271a6
LunarNPC <143328177+AnreHub235@users.noreply.github.com>
2024-03-10 22:19:26 +0700

    Update Link.js

--- API/Link.js
+++ API/Link.js
@@ -1,42 +1,15 @@
-:Codex:
- :IOS:
- https://www.mediafire.com/file/3mbp8j9ozhx0pmm/CodexIOS.ipa/file
- 
- :Android:
- https://www.mediafire.com/file/a4ykl5l0a2negey/Codex_Police.apk/file
+**__OFFICIAL VERSIONS__** [ORIGINAL VERSIONS]
+**LATEST VERSION** **v 2.614** [check6](https://cdn.discordapp.com/emojis/950297012753010698.gif?size=48&quality=lossless&name=check6)
 
-:Fluxus:
- :IOS:
- Not Found
+[deltalogo](https://cdn.discordapp.com/emojis/1175308654023557140.webp?size=48&quality=lossless&name=deltalogo) **__DELTA__** : https://link-center.net/1052601/delta-v2614
+[hydrogenlogo](https://cdn.discordapp.com/emojis/1175308812576620634.webp?size=48&quality=lossless&name=hydrogenlogo) **__HYDROGEN__** : https://direct-link.net/1052601/hydrogen-v2614
+[codexlogo](https://cdn.discordapp.com/emojis/1175308708901830807.webp?size=48&quality=lossless&name=codexlogo) **__CODEX__** : https://direct-link.net/1052601/codex-v2614
+[neologo](https://cdn.discordapp.com/emojis/1187549923491729408.webp?size=48&quality=lossless&name=neologo) **__ARCEUS X NEO__** : https://link-target.net/1052601/arceus-x-neo-v2614
+[evonlogo](https://cdn.discordapp.com/emojis/1192995861832147004.webp?size=48&quality=lossless&name=evonlogo) **__EVON__** : https://direct-link.net/1052601/evon-v2614
+[valyselogo](https://cdn.discordapp.com/emojis/1206869032213684285.webp?size=48&quality=lossless&name=valyselogo) **__VALYSE__** : https://direct-link.net/1052601/valyse-v2614
+[vegaxlogo](https://cdn.discordapp.com/emojis/1209792076758126612.webp?size=48&quality=lossless&name=vegaxlogo) **__VEGA X__** : https://link-target.net/1052601/vega-x-v4-v2614
+[trigonlogo](https://cdn.discordapp.com/emojis/1195893209881124925.webp?size=48&quality=lossless&name=trigonlogo) **__TRIGON__** : https://link-hub.net/1052601/trigon-v2614
 
- :Android:
-     https://www.mediafire.com/file/2q3ss97rehasml2/Original_Fluxus_2.612_32bit.apk/file
-     64-bit died bruh
- 
-:Delta:
- :IOS: 
-     Not Found
- 
- :Android: 
-     https://www.mediafire.com/file/18g50unlpqjcccb/DELTA+2.610.472_signed+aligned.apk/file
-
-:Hydrogen:
- :IOS: 
-     Not Found
-
- :Android: 
-     https://rb.gy/62058
-
-:Vegax:
- :IOS: 
-     Not Found
- 
- :Android: 
-     https://www.mediafire.com/file_premium/6bhmia2v10hf5z7/Vega_X_V4.apk/file
-
-:ArceusX:
- :IOS: 
-     Not Found
-
- :Android: 
-     https://arceusxneoexecutor.com/neo.apk
+-> [fluxuslogo](https://cdn.discordapp.com/emojis/1175308860655943710.webp?size=48&quality=lossless&name=fluxuslogo) **__FLUXUS__**  :
+**__32bit__** : https://link-target.net/901202/official-fluxus-v2614-32
+**__64bit__** : https://direct-link.net/901202/official-fluxus-v2614-64
