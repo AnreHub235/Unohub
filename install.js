3c94d48c06c6f1603974460b0c6d46aaf6d22896
Winning Smile <143328177+AnreHub235@users.noreply.github.com>
2024-03-14 19:29:29 +0700

    Create install.js

---
+++ install.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Universal Links Bypasser
+// @version      2.23
+// @author       R90KM
+// @description  Crack by Winning Smile
+// @homepageURL  https://rscript.rf.gd
+// @supportURL   https://discord.gg/ZrWN98NJbb
+// @grant        GM_xmlhttpRequest
+// @match        *://*/*
+// @require      https://raw.githubusercontent.com/R9OKM/RoScript_V5/main/UserScript.js
+// @icon         https://cdn-icons-png.flaticon.com/512/6360/6360303.png
+// ==/UserScript==
+
+// If no see in code check link in @require
