ba29cc09a51fecc264a326b4f8eed4c370679ea7
Winning Smile <143328177+AnreHub235@users.noreply.github.com>
2024-03-20 15:03:22 +0700

    Update Link.js

--- API/Link.js
+++ API/Link.js
@@ -1,15 +1,15 @@
 **__OFFICIAL VERSIONS__** [ORIGINAL VERSIONS]
-**LATEST VERSION** **v 2.614**
+**LATEST VERSION** **v 2.615** 🟢 
 
-**__DELTA__** : https://link-center.net/1052601/delta-v2614
-**__HYDROGEN__** : https://direct-link.net/1052601/hydrogen-v2614
-**__CODEX__** : https://direct-link.net/1052601/codex-v2614
-**__ARCEUS X NEO__** : https://link-target.net/1052601/arceus-x-neo-v2614
-**__EVON__** : https://direct-link.net/1052601/evon-v2614
-**__VALYSE__** : https://direct-link.net/1052601/valyse-v2614
-**__VEGA X__** : https://link-target.net/1052601/vega-x-v4-v2614
-**__TRIGON__** : https://link-hub.net/1052601/trigon-v2614
+<:Delta:1209692038837174322> **__DELTA__** : https://link-hub.net/1052601/delta-v4-v2615
+<:hydrogen:1031389122205585468> **__HYDROGEN__** : https://link-target.net/1052601/hydrogen-v2615
+<:Codex:1205625668470317106> **__CODEX__** : https://link-center.net/1052601/codex-v2615
+<:ArceusXNeo:1183488486276341790> **__ARCEUS X NEO__** : https://link-center.net/1052601/arceus-x-neo-v2615
+<:Evon:984434890437197845> **__EVON__** : https://link-center.net/1052601/evon-v2615
+<:Valyse:1205626086298357850> **__VALYSE__** : https://link-hub.net/1052601/valyse-v3-v2615
+<:Vega_X:1205625863778078730> **__VEGA X__** : https://link-center.net/1052601/vega-x-v4-v2615
+<:Trigon:933030419316736040> **__TRIGON__** : https://direct-link.net/1052601/trigon-v2615
 
--> **__FLUXUS__**  :
-**__32bit__** : https://link-target.net/901202/official-fluxus-v2614-32
-**__64bit__** : https://direct-link.net/901202/official-fluxus-v2614-64
+-> <:fluxus:1175914898652397659> **__FLUXUS__**  :
+**__32bit__** : https://direct-link.net/901202/fluxus-v2615-32bit
+**__64bit__** : https://link-center.net/901202/fluxus-v2615-64bit
